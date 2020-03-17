0b4780611ce1a7eed04206c5fa547f4fc63deb3c
fj-gruenewald <61251684+fj-gruenewald@users.noreply.github.com>
2020-03-17 16:17:29 +0100

    initial commit
    initial commit

---
+++ ObjectDetection/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ObjectDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GridView = new System.Windows.Forms.DataGridView();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.confidenceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.widthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.heightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yoloItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.picbox = new System.Windows.Forms.PictureBox();
+            this.bttn_load = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.bttn_analyze = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton3 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton4 = new MaterialSkin.Controls.MaterialRaisedButton();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloItemBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridView
+            // 
+            this.GridView.AutoGenerateColumns = false;
+            this.GridView.BackgroundColor = System.Drawing.Color.White;
+            this.GridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.typeDataGridViewTextBoxColumn,
+            this.confidenceDataGridViewTextBoxColumn,
+            this.xDataGridViewTextBoxColumn,
+            this.yDataGridViewTextBoxColumn,
+            this.widthDataGridViewTextBoxColumn,
+            this.heightDataGridViewTextBoxColumn});
+            this.GridView.DataSource = this.yoloItemBindingSource;
+            this.GridView.Enabled = false;
+            this.GridView.Location = new System.Drawing.Point(12, 494);
+            this.GridView.MultiSelect = false;
+            this.GridView.Name = "GridView";
+            this.GridView.Size = new System.Drawing.Size(642, 115);
+            this.GridView.TabIndex = 0;
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // confidenceDataGridViewTextBoxColumn
+            // 
+            this.confidenceDataGridViewTextBoxColumn.DataPropertyName = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.HeaderText = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.Name = "confidenceDataGridViewTextBoxColumn";
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            this.xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            this.xDataGridViewTextBoxColumn.HeaderText = "X";
+            this.xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            this.yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            this.yDataGridViewTextBoxColumn.HeaderText = "Y";
+            this.yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            // 
+            // widthDataGridViewTextBoxColumn
+            // 
+            this.widthDataGridViewTextBoxColumn.DataPropertyName = "Width";
+            this.widthDataGridViewTextBoxColumn.HeaderText = "Width";
+            this.widthDataGridViewTextBoxColumn.Name = "widthDataGridViewTextBoxColumn";
+            // 
+            // heightDataGridViewTextBoxColumn
+            // 
+            this.heightDataGridViewTextBoxColumn.DataPropertyName = "Height";
+            this.heightDataGridViewTextBoxColumn.HeaderText = "Height";
+            this.heightDataGridViewTextBoxColumn.Name = "heightDataGridViewTextBoxColumn";
+            // 
+            // yoloItemBindingSource
+            // 
+            this.yoloItemBindingSource.DataSource = typeof(Alturos.Yolo.Model.YoloItem);
+            // 
+            // picbox
+            // 
+            this.picbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(142)))), ((int)(((byte)(60)))));
+            this.picbox.Location = new System.Drawing.Point(12, 72);
+            this.picbox.Name = "picbox";
+            this.picbox.Size = new System.Drawing.Size(642, 416);
+            this.picbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picbox.TabIndex = 1;
+            this.picbox.TabStop = false;
+            // 
+            // bttn_load
+            // 
+            this.bttn_load.Depth = 0;
+            this.bttn_load.Location = new System.Drawing.Point(660, 72);
+            this.bttn_load.MouseState = MaterialSkin.MouseState.HOVER;
+            this.bttn_load.Name = "bttn_load";
+            this.bttn_load.Primary = true;
+            this.bttn_load.Size = new System.Drawing.Size(170, 36);
+            this.bttn_load.TabIndex = 3;
+            this.bttn_load.Text = "Load";
+            this.bttn_load.UseVisualStyleBackColor = true;
+            this.bttn_load.Click += new System.EventHandler(this.bttn_load_Click);
+            // 
+            // bttn_analyze
+            // 
+            this.bttn_analyze.Depth = 0;
+            this.bttn_analyze.Location = new System.Drawing.Point(660, 114);
+            this.bttn_analyze.MouseState = MaterialSkin.MouseState.HOVER;
+            this.bttn_analyze.Name = "bttn_analyze";
+            this.bttn_analyze.Primary = true;
+            this.bttn_analyze.Size = new System.Drawing.Size(170, 36);
+            this.bttn_analyze.TabIndex = 4;
+            this.bttn_analyze.Text = "Analyze";
+            this.bttn_analyze.UseVisualStyleBackColor = true;
+            this.bttn_analyze.Click += new System.EventHandler(this.bttn_analyze_Click);
+            // 
+            // materialRaisedButton3
+            // 
+            this.materialRaisedButton3.Depth = 0;
+            this.materialRaisedButton3.Location = new System.Drawing.Point(660, 410);
+            this.materialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton3.Name = "materialRaisedButton3";
+            this.materialRaisedButton3.Primary = true;
+            this.materialRaisedButton3.Size = new System.Drawing.Size(170, 36);
+            this.materialRaisedButton3.TabIndex = 7;
+            this.materialRaisedButton3.Text = "Clear";
+            this.materialRaisedButton3.UseVisualStyleBackColor = true;
+            // 
+            // materialRaisedButton4
+            // 
+            this.materialRaisedButton4.Depth = 0;
+            this.materialRaisedButton4.Location = new System.Drawing.Point(660, 452);
+            this.materialRaisedButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton4.Name = "materialRaisedButton4";
+            this.materialRaisedButton4.Primary = true;
+            this.materialRaisedButton4.Size = new System.Drawing.Size(170, 36);
+            this.materialRaisedButton4.TabIndex = 8;
+            this.materialRaisedButton4.Text = "Close";
+            this.materialRaisedButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 615);
+            this.Controls.Add(this.materialRaisedButton4);
+            this.Controls.Add(this.materialRaisedButton3);
+            this.Controls.Add(this.bttn_analyze);
+            this.Controls.Add(this.bttn_load);
+            this.Controls.Add(this.picbox);
+            this.Controls.Add(this.GridView);
+            this.Name = "Form1";
+            this.Text = "Object Detector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloItemBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GridView;
+        private System.Windows.Forms.PictureBox picbox;
+        private MaterialSkin.Controls.MaterialRaisedButton bttn_load;
+        private MaterialSkin.Controls.MaterialRaisedButton bttn_analyze;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn confidenceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn widthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn heightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource yoloItemBindingSource;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton3;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton4;
+    }
+}
+
---
+++ ObjectDetection/Form1.cs
@@ -0,0 +1,105 @@
+﻿using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ObjectDetection
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green700, Primary.Green900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void bttn_load_Click(object sender, EventArgs e)
+        {
+            // FileDialog öffnen um Bild auszuwählen (dabei werden formate beachtet ist aber eigentlich egal)
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPG|*.jpg|PNG|*.png|JPEG|*.jpeg" })
+            {
+                // wenn der Dialog ein akzeptiertes Bild erkannt hat wird dies in der Picturebox angezeigt
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    picbox.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+        }
+
+        private void bttn_analyze_Click(object sender, EventArgs e)
+        {
+            // vorgefertigter code aus der readme des nuget paketes
+            var configurationDetector = new ConfigurationDetector();
+            var config = configurationDetector.Detect();
+            using (var yoloWrapper = new YoloWrapper(config))
+            {
+                // umwandeln in einen 2bit stream 
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    picbox.Image.Save(ms,System.Drawing.Imaging.ImageFormat.Png);
+                    var items = yoloWrapper.Detect(ms.ToArray()).ToList();
+                    yoloItemBindingSource.DataSource = items;
+                    AddDetailsToPicturebox(picbox, items);
+                }
+
+                //items[0].Type -> "Person, Car, ..."
+                //items[0].Confidence -> 0.0 (low) -> 1.0 (high)
+                //items[0].X -> bounding box
+                //items[0].Y -> bounding box
+                //items[0].Width -> bounding box
+                //items[0].Height -> bounding box
+            }
+        }
+
+        //Methode zum rendern des dreiecks
+        void AddDetailsToPicturebox(PictureBox pictureBoxToRender, List<YoloItem> items)
+        {
+            //grafik aus dem bild erstellen
+            var img = pictureBoxToRender.Image;
+
+            //font zum schreiben des textes der objekte brush für das eigentliche schreiben
+            var font = new Font("Arial", 20, FontStyle.Bold);
+            var brush = new SolidBrush(Color.LightGreen);
+
+            var graphics = Graphics.FromImage(img);
+            foreach(var item in items)
+            {
+                //für die x und y werte der ekannten objekte variablen anlegen
+                var x = item.X;
+                var y = item.Y;
+                var width = item.Width;
+                var height = item.Height;
+
+                //ein rectangle zeichnen und einen pen zum zeichnen deklarieren
+                var rect = new Rectangle(x, y, width, height);
+                var pen = new Pen(Color.LightGreen, 6);
+
+                //punkt für das schreiben des textes
+                var point = new Point(x, y);
+
+                //das eigentliche zeichnen des quadrates
+                graphics.DrawRectangle(pen, rect);
+
+                //schreibes des textes in die rectangles
+                graphics.DrawString(item.Type, font, brush, point);
+            }
+
+            //abspeichern des quadrates in einer bitmap zum anwenden auf die picturebox
+            pictureBoxToRender.Image = img;
+        }
+    }
+}
